e6636104496aa5c45197d057381c2f98a6e5a19a
Wilhelm Klopp <wil.klopp@gmail.com>
2019-08-30 11:11:01 +0100

    fix spaces

--- index.test.js
+++ index.test.js
@@ -5,19 +5,19 @@ test('adds 1 + 2 to equal 3', () => {
 })
 
 
-// Lorem ipsum dolor sit amet, 
-// consectetur adipiscing elit. Etiam hendrerit finibus lacus sit amet consequat. 
-// Vivamus ut vulputate augue, vitae imperdiet velit. Aliquam eget 
-// hendrerit mi. In vel mattis nisl. Quisque ultrices feugiat nibh, id tincidunt est mollis eu. 
-// Duis finibus rutrum hendrerit. Mauris ornare rhoncus libero a maximus. 
-// Mauris egestas nunc a lorem lobortis, eget elementum libero feugiat. 
+// Lorem ipsum dolor sit amet,
+// consectetur adipiscing elit. Etiam hendrerit finibus lacus sit amet consequat.
+// Vivamus ut vulputate augue, vitae imperdiet velit. Aliquam eget
+// hendrerit mi. In vel mattis nisl. Quisque ultrices feugiat nibh, id tincidunt est mollis eu.
+// Duis finibus rutrum hendrerit. Mauris ornare rhoncus libero a maximus.
+// Mauris egestas nunc a lorem lobortis, eget elementum libero feugiat.
 // Vivamus eu mi ipsum. Proin efficitur quis velit a dignissim. Sed in nisl eleifend, dignissim nulla vel, pharetra odio. Integer eget mauris euismod, vulputate est id, volutpat felis. Vivamus non vestibulum mauris, quis vulputate purus.
 
-// Donec eget faucibus nisi. Proin odio ligula, 
+// Donec eget faucibus nisi. Proin odio ligula,
 // fringilla quis massa eget, venenatis elementum magna. Donec et hendrerit justo. Nunc neque magna, vulputate vitae leo ac, tempus dapibus leo. Integer vulputate velit vel odio feugiat facilisis. Fusce bibendum est eu iaculis varius. Suspendisse ac nulla molestie, euismod velit eu, interdum arcu. Suspendisse ultricies massa vel libero vulputate, vitae convallis nisi suscipit. Donec eu semper neque. Aenean tellus odio, faucibus sed ligula vel, laoreet gravida neque. Proin vel libero mattis, ullamcorper massa a, maximus tellus. Aenean ut fermentum nisi, eu pulvinar velit. Etiam eget nisi sem. Ut tincidunt congue lacus id auctor.
 
-// Praesent nec malesuada lacus, eu condimentum magna. Praesent a lacus lectus. 
-// Suspendisse varius ligula vitae felis commodo, volutpat pretium ipsum sagittis. 
-// Nullam eget orci tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada 
-// fames ac turpis egestas. Fusce rhoncus, est eget euismod tincidunt, 
+// Praesent nec malesuada lacus, eu condimentum magna. Praesent a lacus lectus.
+// Suspendisse varius ligula vitae felis commodo, volutpat pretium ipsum sagittis.
+// Nullam eget orci tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada
+// fames ac turpis egestas. Fusce rhoncus, est eget euismod tincidunt,
 //   turpis quam tristique quam, non laoreet tortor urna et tortor. Vestibulum consequat, metus at rhoncus interdum, nisi eros congue orci, at tristique tellus augue sit amet turpis. Ut venenatis risus a quam rutrum, id feugiat dui volutpat.
